025cf06d5f1e945324e9cddcc2c7906c5eae7b45
porat-ah <porat2001@gmail.com>
2021-03-14 14:26:23 +0200

    short function + added main

--- Demo.cpp
+++ Demo.cpp
@@ -1,46 +1,52 @@
-/**
- * Demo program for snowman exercise.
- * 
- * Author: Erel Segal-Halevi
- * Since : 2021-02
- */
 
 #include "snowman.hpp"
-
 #include <iostream>
 #include <stdexcept>
+#include <array>
+#include <stdlib.h>
 using namespace std;
+#define print(x) cout << x << endl
 
-int main() {
-	cout << ariel::snowman(11114411) << endl;   /* Should print:
-_===_
-(.,.)
-( : )
-( : )
-	*/
-	cout << ariel::snowman(33232124) << endl;   /* Should print:
-   _
-  /_\
-\(o_O)
- (] [)>
- (   )
-	*/
-	try {
-		cout << ariel::snowman(5) << endl;   // Exception - not a valid code
-	} catch (exception& ex) {
-	 	cout << "   caught exception: " << ex.what() << endl;  // should print "Invalid code '5'"
-	}
-	for (size_t i = 1; i < 5; i++)
+string name_generator(){
+	string out ;
+	for (size_t i = 0; i < 8; i++)
 	{
-		int temp = i;
-		for (size_t j = 0; j < 7; j++)
-		{
-			temp *=10;
-			temp +=i;
-		}
-		cout << temp << endl;
-		cout << ariel::snowman(temp) << endl;
+		int num = (rand()%27+97);
+		char c = (char) num;
+		out += c;
 	}
-
+	return out;
+}
+int main() {
 	
+	array<int ,27> map = {1, 3, 4, 4, 1, 1, 3, 2, 3, 1, 3, 4, 4, 3, 2, 3, 2, 2, 2, 4, 4, 4,2, 3, 2, 1, 4};
+	string name;
+	cout << "welcome to snowman generator "<< endl; 
+	cout << "here you can create your own snowman "<< endl; 
+	cout << "you only need to give it a name we will do the rest "<< endl;
+	cout << "if you want to use the name genrator enter : 0 "<< endl;
+	cout << "if you want to enter your own name enter : 1 "<< endl;
+	int input;
+	cin >> input;
+	switch (input)
+	{
+	case 0 :
+		name = name_generator();
+		print(name);
+		break;
+	case 1:
+		cout << "enter the snowman name ( must be 8 letters)" << endl;
+		cin >> name;
+		break;
+	}
+	int code =0 ;
+	for (size_t i = 0; i < 8; i++)
+	{
+		code *= 10;
+		code += map.at(name.at(i)-97);
+	}
+	print("this is " + name);
+	print("say hello to him");
+	print(ariel::snowman(code));
+	print(name +"'s id = " + to_string(code));
 }
--- snowman.cpp
+++ snowman.cpp
@@ -4,81 +4,60 @@
 #include <iostream> 
 #include <array>
 
+using namespace std;
 namespace ariel
 {
-    std::string snowman(int code){
-        const int PART_NUMBER = 7 ;
-        const int SNOWMAN_LEN = 10 ;
-        const int CODE_LEN = 8;
+    void check(int code){
         const int MIN_CODE = 11111111;
         const int MAX_CODE = 44444444;
-        const int MIN_NUM_MINUS_1 = 0;
-        const int MAX_NUM_MINUS_1 = 3;
-        const int INIT = 0;
-        const int TEN = 10 ;
-        const int RIGHT_ARM = 5 ;
-        const int RIGHT_ARM_REPLACE = 8 ;
-        const int LEFT_ARM = 4;
-        const int LEFT_ARM_REPLACE = 9 ;
-
         if (code <MIN_CODE || code > MAX_CODE)
         {
             throw std::invalid_argument{"code length incorrect "};
         }
-        //HNLRXYTB
-        // HHHHH
-        // HHHHH
-        //X(LNR)Y
-        //X(TTT)Y
-        // (BBB)
-        const std::array<std::array<std::string,4>,8> parts = {{
+    }
+    //HNLRXYTB
+    // HHHHH
+    // HHHHH
+    //X(LNR)Y
+    //X(TTT)Y
+    // (BBB)
+    string snowman(int code){
+        check(code);
+        const int MIN_NUM_MINUS_1 = 0;
+        const int MAX_NUM_MINUS_1 = 3;
+        const int INIT = 0;
+        const int TEN = 10 ;
+        const int CODE_LEN = 8;
+        array<int , CODE_LEN> HNLRXYTB = {};
+        int _code = code;
+        for (int i = CODE_LEN -1; i > -1; --i)
+        {
+            HNLRXYTB.at(i) = (code %TEN) -1;
+            if ( HNLRXYTB.at(i) < MIN_NUM_MINUS_1 || HNLRXYTB.at(i) > MAX_NUM_MINUS_1)
+            {
+                throw invalid_argument{ " Invalid code " + to_string(_code) };
+            }
+            code /= TEN;
+        }
+        const array<array<string,4>,10> parts = {{
             {" _===_ ","  ___ \n ..... ","   _\n  /_\\","  ___\n (_*_)"},// hats - H
             {",",".","_"," "},//nose - N
             {"(.","(o","(O","(-"},//left eye - L
             {".)","o)","O)","-)"},//right eye - R
-            {"\n<","\n\\","\n/","\n "},//left arm - X
-            {">","/","\\"," "},//right arm - Y
+            {"\n<","\n ","\n/","\n "},//left arm - X
+            {">"," ","\\"," "},//right arm - Y
             {"( : )","(] [)","(> <)","(   )"},//torso - T
-            {"\n ( : )","\n (\" \")","\n (___)","\n (   )"}//base - B
+            {"\n ( : )","\n (\" \")","\n (___)","\n (   )"},//base - B
+            {"\n ","\n\\","\n ","\n "},// second left arm 
+            {" ", "/", " ", " "}// second right arm
         }};
-        int part_type = INIT;
-        int part = PART_NUMBER;
-        std::array<std::string,SNOWMAN_LEN> snowman;
-        for (size_t i = INIT; i < CODE_LEN; i++)
-        {
-            part_type = code %TEN -1;
-            if (part_type < MIN_NUM_MINUS_1 || part_type > MAX_NUM_MINUS_1)
-            {
-                throw std::invalid_argument{ " Invalid code " + std::to_string(code) };
-            }
-            
-            code /= TEN;
-            snowman.at(part) = parts.at(part).at(part_type);
-            part--;
-        }
-        if (snowman.at(RIGHT_ARM) == "/" )
-        {
-            snowman.at(RIGHT_ARM_REPLACE) = "/";
-            snowman.at(RIGHT_ARM) = " ";
-        }
-        else
-        {
-            snowman.at(RIGHT_ARM_REPLACE) = " ";
-        }
-        if (snowman.at(LEFT_ARM) == "\n\\" )
-        {
-            snowman.at(LEFT_ARM_REPLACE) = "\n\\";
-            snowman.at(LEFT_ARM) = "\n ";
-        }
-        else
-        {
-            snowman.at(LEFT_ARM_REPLACE) = "\n ";
-        }
-        const std::array<int,10> order = {0,9,2,1,3,8,4,6,5,7};
-        std::string out;
+        const array<int,10> order = {0,8,2,1,3,9,4,6,5,7};
+        const array<int,10> part_order = {0,4,2,1,3,5,4,6,5,7};
+        string out;
         for (size_t i = INIT; i < TEN; i++)
         {
-            out += snowman.at(order.at(i));
+            int part =  HNLRXYTB.at(part_order.at(i));
+            out += parts.at(order.at(i)).at(part);
         }
         return out;
     }
